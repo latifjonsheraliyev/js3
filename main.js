f3f28db5c3fc3b4e35554b11230b92e93490d6e2
latif <latifjonsheraliyev209@gmail.com>
2024-09-19 09:26:52 +0500

    js3

---
+++ main.js
@@ -0,0 +1,38 @@
+// function mergeObjects(obj1, obj2) {
+//   return { ...obj1, ...obj2 };
+// }
+
+// const obj1 = { a: 1, b: 2 };
+// const obj2 = { b: 3, c: 4 };
+
+// const merged = mergeObjects(obj1, obj2);
+// console.log(merged);
+
+// function sumObjectValues(obj) {
+//     return Object.values(obj).reduce((sum, value) => sum + value, 0);
+// }
+
+// const obj1 = { a: 10, b: 20, c: 30 };
+// console.log(sumObjectValues(obj1)); // 60
+
+// function getObjectKeys(obj) {
+//     return Object.keys(obj);
+// }
+
+// const obj2 = { a: 1, b: 2, c: 3 };
+// console.log(getObjectKeys(obj2));
+
+// function getObjectValues(obj) {
+//     return Object.values(obj);
+// }
+
+// const obj3 = { a: 1, b: 2, c: 3 };
+// console.log(getObjectValues(obj3));
+
+// function hasKey(obj, key) {
+//     return obj.hasOwnProperty(key);
+// }
+
+// const obj4 = { a: 1, b: 2, c: 3 };
+// console.log(hasKey(obj4, 'b'));
+// console.log(hasKey(obj4, 'd'));
